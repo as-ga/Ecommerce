fceb0afb2e7dc6e4e9ae4aa6fdeca4a34f2d96d6
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-03-03 12:24:31 +0530

    update readme

--- src/components/testimonial/Testimonial.jsx
+++
@@ -1,9 +0,0 @@
-function Testimonial() {
-  return (
-    <div>
-      <h1 className="mb-10 text-center text-2xl font-bold">Testimonial</h1>
-    </div>
-  );
-}
-
-export default Testimonial;
--- src/pages/order/Order.jsx
+++
@@ -1,9 +0,0 @@
-import Layout from "../../components/layout/Layout";
-
-export default function Order() {
-  return (
-    <Layout>
-      <h2 className="mb-10 text-center text-2xl font-bold">Not Order</h2>
-    </Layout>
-  );
-}
