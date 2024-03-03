3c2297eaf6f4d33cc0e5581f88a30e114e079939
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-03-04 01:39:23 +0530

    shadcn

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "./styles/app.scss";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
