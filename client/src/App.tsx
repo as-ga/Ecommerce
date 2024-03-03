a4529dbb442b86de8949e41947f8024df1e7fe18
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-03-03 12:31:22 +0530

    initialize client site

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>App</h1>
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
